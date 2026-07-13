12ea136ebde0b36ace7e6a9c4345b6cecce7766b
Harold Vinueza <harold.vinueza2016@uteq.edu.ec>
2026-07-12 20:46:17 -0500

    feat(academico-laboratorios-service): estructura base, pom.xml, application.yml y migracion Flyway V1

---
+++ academico-laboratorios-service/src/main/java/ec/edu/scli/academico/AcademicoLaboratoriosServiceApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.scli.academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoLaboratoriosServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicoLaboratoriosServiceApplication.class, args);
+    }
+
+}
